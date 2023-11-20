b2b5f95f0899fd967ed6b5cf4f98a352811b7a04
Alexandre PETIT <alexbreuillet@gmail.com>
2023-11-20 20:26:37 +0100

    Add default conf and build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    resolve: {
+        alias: [
+            {find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))},
+            {find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url))},
+            {find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url))},
+            {find: '@vues', replacement: fileURLToPath(new URL('./src/vue', import.meta.url))},
+        ],
+    }
 })
