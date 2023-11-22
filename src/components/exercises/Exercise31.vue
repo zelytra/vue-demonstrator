cbcf1589a40a2406d68af8a55a4172f89f7a9c3e
Alexandre PETIT <alexbreuillet@gmail.com>
2023-11-22 09:14:51 +0100

    Correct build problem on exercise 3

--- src/components/exercises/Exercise31.vue
+++ src/components/exercises/Exercise31.vue
@@ -2,9 +2,10 @@
   <ExerciseDisplayer title="Exercise3.1.vue">
     <template #content>
       <div class="content">
-        <input type="range" @input="slider=Number.parseInt($event.target.value)" :value="slider">
+        <input type="range" @input="updateSliderValue" :value="slider">
         <p>I'm Exercise3.vue child ! And i receive props and i cannot change them !</p>
-        <a href="https://vuejs.org/guide/components/props.html#one-way-data-flow" target="_blank">Vue3 Documentation : Props</a>
+        <a href="https://vuejs.org/guide/components/props.html#one-way-data-flow" target="_blank">Vue3 Documentation :
+          Props</a>
       </div>
     </template>
     <template #code>
@@ -19,6 +20,11 @@ import ExerciseDisplayer from "@/vue/layouts/ExerciseDisplayer.vue";
 const props = defineProps({
   slider: Number
 })
+
+function updateSliderValue(event: any) {
+  //@ts-ignore For educational purpose only
+  props.slider = event.target.value
+}
 </script>
 
 <style scoped lang="scss">
